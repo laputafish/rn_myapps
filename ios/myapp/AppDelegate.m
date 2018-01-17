ce765703ebf18c0d319c5581b2542c43ca826142
Dominic Lee <leemflee@gmail.com>
2018-01-17 08:28:24 +0800

    added map to ios/POD

--- ios/myapp/AppDelegate.m
+++ ios/myapp/AppDelegate.m
@@ -11,11 +11,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps; //add this line if you want to use Google Maps
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAbuAORA8x4MeAOQ19eLbtgP13sBS3tdeQ"];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
